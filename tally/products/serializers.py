141fefc7b6722e5b474c611e61e8f979c6d9a893
larsvantol <LMJvanTol@gmail.com>
2023-03-09 22:15:33 +0100

    Added API endpoints for product groups

--- tally/products/serializers.py
+++ tally/products/serializers.py
@@ -32,8 +32,34 @@ class ProductSerializer(serializers.ModelSerializer):
                                         )
         else:
             raise serializers.ValidationError(f"product_group_id: Product group with id {validated_data['product_group_id']} does not exist")
+        
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Product` instance, given the validated data.
+        """
+
+        # Check if stock is in validated_data, if not set stock to current stock
+        if 'stock' not in validated_data:
+            validated_data['stock'] = instance.stock
+        # Check if image_url is in validated_data, if not set image_url to current image_url
+        if 'image_url' not in validated_data:
+            validated_data['image_url'] = instance.image_url
+
+        # Check if a product group with primary key product_group_id exists
+        product_group = ProductGroup.objects.filter(pk=validated_data['product_group_id'])
+        if product_group.exists():
+            instance.name = validated_data.get('name', instance.name)
+            instance.price = validated_data.get('price', instance.price)
+            instance.stock = validated_data.get('stock', instance.stock)
+            instance.image_url = validated_data.get('image_url', instance.image_url)
+            instance.product_group = product_group.get()
+            instance.save()
+            return instance
+        else:
+            raise serializers.ValidationError(f"product_group_id: Product group with id {validated_data['product_group_id']} does not exist")
 
     class Meta:
         model = Product
         fields = ['id', 'name', 'price', 'stock', 'image_url', 'product_group', 'product_group_id']
+        extra_kwargs = {'stock': {'required': False}}
         depth = 1
--- tally/products/views.py
+++ tally/products/views.py
@@ -8,36 +8,48 @@ from rest_framework.decorators import action
 from .models import Product, ProductGroup
 from .serializers import ProductSerializer, ProductGroupSerializer
 
+GET_LIST = "list"
+GET_RETRIEVE = "retrieve"
+POST_CREATE = "create"
+DELETE_DESTROY = "destroy"
+PUT_UPDATE = "update"
+PATCH_PARTIAL_UPDATE = "partial_update"
+
 class ProductGroupViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing product groups.
     """
+    
     queryset = ProductGroup.objects.all()
-    serializer_class = ProductGroupSerializer
+    serializer_class = ProductGroupSerializer 
 
     def get_permissions(self):
         """
         Instantiates and returns the list of permissions that this view requires.
         """
-        if self.action == 'list':
+        
+        if self.action == GET_LIST:
+            permission_classes = [AllowAny]
+        elif self.action == GET_RETRIEVE:
+            permission_classes = [AllowAny]
+        elif self.action == POST_CREATE:
+            permission_classes = [AllowAny]
+        elif self.action == PUT_UPDATE:
+            permission_classes = [AllowAny]
+        elif self.action == DELETE_DESTROY:
             permission_classes = [AllowAny]
         else:
             permission_classes = [IsAdminUser]
+
         return [permission() for permission in permission_classes]
 
 class ProductViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing products.
     """
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        """
-        Returns all products
-        """
-        queryset = Product.objects.all()
-        return queryset
 
     def list(self, request):
         """
@@ -51,10 +63,20 @@ class ProductViewSet(viewsets.ModelViewSet):
         """
         Instantiates and returns the list of permissions that this view requires.
         """
-        if self.action == 'list':
+
+        if self.action == GET_LIST:
             permission_classes = [AllowAny]
-        elif self.action == 'create':
+        elif self.action == GET_RETRIEVE:
+            permission_classes = [AllowAny]
+        elif self.action == POST_CREATE:
+            permission_classes = [AllowAny]
+        elif self.action == PUT_UPDATE:
+            permission_classes = [AllowAny]
+        elif self.action == PATCH_PARTIAL_UPDATE:
+            permission_classes = [AllowAny]
+        elif self.action == DELETE_DESTROY:
             permission_classes = [AllowAny]
         else:
             permission_classes = [IsAdminUser]
+
         return [permission() for permission in permission_classes]
