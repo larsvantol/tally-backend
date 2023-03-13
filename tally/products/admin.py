0bf2693c1054e3fad8e2368e4b46a9a1276fc6ae
larsvantol <LMJvanTol@gmail.com>
2023-03-13 07:34:08 +0100

    Admin (#6)
    * added filters to products
    
    * Improved admin pages

--- tally/products/admin.py
+++ tally/products/admin.py
@@ -1,11 +1,77 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from math import log, exp
 from .models import Product, ProductGroup
+
+# Filter for stock using simplelistfilter
+class StockFilter(admin.SimpleListFilter):
+    title = 'stock'
+    parameter_name = 'stock'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('0-10', 'Less than 10'),
+            ('10-50', '10-50'),
+            ('50-100', '50-100'),
+            ('100-500', '100-500'),
+            ('500>', 'Greater than 500'),
+        )
+    
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        elif self.value() == '500>':
+            return queryset.filter(stock__gte=500)
+        else:
+            min, max = self.value().split('-')
+            return queryset.filter(stock__gte=min, stock__lte=max)
+        
+
 
 class ProductGroupAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    search_fields = ('name',)
+    fieldsets = (
+        ('Name', {
+            'fields': ('name',)
+        }),
+        ('Meta', {
+            'fields': ('created', 'last_modified')
+        }),
+    )
+    readonly_fields = ('created', 'last_modified')
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
+    list_display = ('name', 'product_group_formatted', 'price_formatted', 'stock')
+    list_filter = ('product_group', StockFilter)
+    search_fields = ('name',)
+    fieldsets = (
+        ('Name', {
+            'fields': ('name', 'product_group')
+        }),
+        ('Details', {
+            'fields': ('price', 'stock', 'image_url')
+        }),
+        ('Meta', {
+            'fields': ('created', 'last_modified')
+        }),
+    )
+    readonly_fields = ('created', 'last_modified')
+
+    @admin.display(description='Price')
+    def price_formatted(self, obj):
+        """
+        Formats the price of the product as €xx.xx
+        """
+        return f'€ {obj.price:.2f}'
+    
+    @admin.display(description='Product group')
+    def product_group_formatted(self, obj):
+        """
+        Formats the product group. It returns a link to the products filtered on the specific product group
+        """
+        # formats the product group with a link to the group filter
+        return format_html(f'<a href="?product_group__id__exact={obj.product_group.id}">{obj.product_group.name}</a>')
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductGroup, ProductGroupAdmin)
--- tally/transactions/admin.py
+++ tally/transactions/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from datetime import datetime
 from .models import Customer, Transaction, SubTransaction, SubPurchase
 
+# Filter transactions based on dates, day, month, year
+class TransactionDateListFilter(admin.SimpleListFilter):
+    title = 'date'
+    parameter_name = 'date'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('today', 'Today'),
+            ('this_week', 'This week'),
+            ('this_month', 'This month'),
+            ('this_year', 'This year'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'today':
+            return queryset.filter(date__date=datetime.today())
+        if self.value() == 'this_week':
+            return queryset.filter(date__week=datetime.today().isocalendar()[1])
+        if self.value() == 'this_month':
+            return queryset.filter(date__month=datetime.today().month)
+        if self.value() == 'this_year':
+            return queryset.filter(date__year=datetime.today().year)
 
 class SubTransactionInline(admin.TabularInline):
     model = SubTransaction
@@ -13,11 +36,38 @@ class SubPurchaseInline(admin.TabularInline):
     extra = 0
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'prefix', 'last_name')
+    list_display = ('full_name', 'relation_code')
+    search_fields = ('first_name', 'prefix', 'last_name', 'relation_code')
+    fieldsets = (
+        ('Name', {
+            'fields': ('first_name', 'prefix', 'last_name')
+        }),
+        ('Meta', {
+            'fields': ('created', 'last_modified')
+        }),
+        ('Exact', {
+            'classes': ('collapse',),
+            'fields': ('relation_code',),
+        }),
+    )
+    readonly_fields = ('created', 'last_modified')
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('transaction_id', 'customer_formatted', 'date_created', 'total_formatted')
+    list_display = ('transaction_id', 'customer_formatted', 'date', 'total_formatted')
+    ordering = ('-date',)
+    search_fields = ('transaction_id', 'customer__first_name', 'customer__prefix', 'customer__last_name')
+    list_filter = (TransactionDateListFilter,)
     inlines = [SubTransactionInline, SubPurchaseInline]
+    autocomplete_fields = ['customer']
+    fieldsets = (
+        ('Information', {
+            'fields': ('transaction_id', 'date', 'customer')
+        }),
+        ('Meta', {
+            'fields': ('created', 'last_modified')
+        }),
+    )
+    readonly_fields = ('transaction_id', 'created', 'last_modified')
 
     @admin.display(description='Total')
     def total_formatted(self, obj):
