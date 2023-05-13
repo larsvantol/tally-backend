12309a742db64df10b142dc940b060b3fad1b977
larsvantol <LMJvanTol@gmail.com>
2023-05-13 20:06:13 +0200

    added endpoint for nicely formatted list

--- tally/transactions/serializers.py
+++ tally/transactions/serializers.py
@@ -2,46 +2,62 @@ from rest_framework import serializers
 from .models import Customer, Transaction, SubTransaction, SubPurchase
 from products.serializers import ProductSerializer
 
+
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'first_name', 'prefix', 'last_name', 'relation_code']
-        extra_kwargs = {
-            'relation_code': {'write_only': True}
-        }
+        fields = ["id", "first_name", "prefix", "last_name", "relation_code"]
+        extra_kwargs = {"relation_code": {"write_only": True}}
+
 
 class SubTransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubTransaction
-        fields = ['description', 'amount']	
+        fields = ["description", "amount"]
+
 
 class SubPurchaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubPurchase
-        fields = ['product', 'quantity', 'price', 'amount']	
-        extra_kwargs = {'price': {'read_only': True}}
+        fields = ["product", "quantity", "price", "amount"]
+        extra_kwargs = {"price": {"read_only": True}}
+
 
 class TransactionSerializer(serializers.ModelSerializer):
-    subtransactions = SubTransactionSerializer(many=True, read_only=False, required=False)
+    subtransactions = SubTransactionSerializer(
+        many=True, read_only=False, required=False
+    )
     subpurchases = SubPurchaseSerializer(many=True, read_only=False, required=False)
 
     class Meta:
         model = Transaction
-        fields = ['transaction_id', 'customer', 'date', 'subtransactions', 'subpurchases']
+        fields = [
+            "transaction_id",
+            "customer",
+            "date",
+            "subtransactions",
+            "subpurchases",
+        ]
 
     def create(self, validated_data):
-        if not validated_data.get('subtransactions') and not validated_data.get('subpurchases'):
-            raise serializers.ValidationError('Transaction must have at least one subtransaction or subpurchase')
+        if not validated_data.get("subtransactions") and not validated_data.get(
+            "subpurchases"
+        ):
+            raise serializers.ValidationError(
+                "Transaction must have at least one subtransaction or subpurchase"
+            )
 
-        subtransactions_data = validated_data.pop('subtransactions', [])
-        subpurchases_data = validated_data.pop('subpurchases', [])
+        subtransactions_data = validated_data.pop("subtransactions", [])
+        subpurchases_data = validated_data.pop("subpurchases", [])
 
         transaction = Transaction.objects.create(**validated_data)
 
         for subtransaction_data in subtransactions_data:
-            SubTransaction.objects.create(transaction=transaction, **subtransaction_data)
+            SubTransaction.objects.create(
+                transaction=transaction, **subtransaction_data
+            )
 
         for subpurchase_data in subpurchases_data:
             SubPurchase.objects.create(transaction=transaction, **subpurchase_data)
-        
+
         return transaction
--- tally/transactions/views.py
+++ tally/transactions/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
-from .models import Customer, Transaction
+from .models import Customer, Transaction, SubTransaction, SubPurchase
 from .serializers import CustomerSerializer, TransactionSerializer
 
 GET_LIST = "list"
@@ -17,44 +17,46 @@ DELETE_DESTROY = "destroy"
 PUT_UPDATE = "update"
 PATCH_PARTIAL_UPDATE = "partial_update"
 
+
 class CustomerViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing customers.
     """
-    
-    @action(detail=True, methods=['get', 'post'])
+
+    @action(detail=True, methods=["get", "post"])
     def transactions(self, request, pk=None):
         """
         Lists transactions for a customer
         """
-        
-        # Check if method is GET
-        if request.method == 'GET':
-            queryset = Transaction.objects.filter(customer__id=pk).order_by('date')
-            serializer = TransactionSerializer(queryset, many=True)
-            return Response(serializer.data)
-        elif request.method == 'POST':
-            if 'start_date' not in request.data:
-                return Response('Missing start_date', status=400)
-            if 'end_date' not in request.data:
-                return Response('Missing end_date', status=400)
-            
-            start_date = request.data.get('start_date')
-            end_date = request.data.get('end_date')
 
-            queryset = Transaction.objects.filter(customer__id=pk, 
-                                                  date__range=[start_date, end_date]).order_by('date')
+        # Check if method is GET
+        if request.method == "GET":
+            queryset = Transaction.objects.filter(customer__id=pk).order_by("date")
             serializer = TransactionSerializer(queryset, many=True)
             return Response(serializer.data)
-    
+        elif request.method == "POST":
+            if "start_date" not in request.data:
+                return Response("Missing start_date", status=400)
+            if "end_date" not in request.data:
+                return Response("Missing end_date", status=400)
+
+            start_date = request.data.get("start_date")
+            end_date = request.data.get("end_date")
+
+            queryset = Transaction.objects.filter(
+                customer__id=pk, date__range=[start_date, end_date]
+            ).order_by("date")
+            serializer = TransactionSerializer(queryset, many=True)
+            return Response(serializer.data)
+
     queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer 
+    serializer_class = CustomerSerializer
 
     def get_permissions(self):
         """
         Instantiates and returns the list of permissions that this view requires.
         """
-        
+
         if self.action == GET_LIST:
             permission_classes = [AllowAny]
         elif self.action == GET_RETRIEVE:
@@ -72,6 +74,7 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
         return [permission() for permission in permission_classes]
 
+
 class TransactionViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing products.
@@ -84,10 +87,48 @@ class TransactionViewSet(viewsets.ModelViewSet):
         """
         Lists products sorted by product group
         """
-        queryset = self.get_queryset().order_by('date')
+        queryset = self.get_queryset().order_by("date")
         serializer = TransactionSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
+    @action(detail=False, methods=["get"])
+    def list_transactions(self, request):
+        """
+        Lists transactions for a customer
+        """
+        list_of_transactions = []
+
+        for transaction in self.get_queryset():
+            for sub_transaction in SubTransaction.objects.filter(
+                transaction=transaction
+            ):
+                list_of_transactions.append(
+                    {
+                        "name": sub_transaction.description,
+                        "quantity": 1,
+                        "amount": sub_transaction.amount,
+                        "date": transaction.date,
+                    }
+                )
+            for sub_purchase in SubPurchase.objects.filter(transaction=transaction):
+                list_of_transactions.append(
+                    {
+                        "name": sub_purchase.product.name,
+                        "quantity": sub_purchase.quantity,
+                        "amount": sub_purchase.amount(),
+                        "date": transaction.date,
+                    }
+                )
+
+        return Response(list_of_transactions)
+
+    #     [
+    #   {name: 'Doritos', quantity: 1, amount: 0.50, date: new Date()},
+    #   {name: 'Croky Paprika Chips', quantity: 1, amount: 0.50, date: new Date("2023-03-23")},
+    #   {name: 'Doritos', quantity: 1, amount: 0.50, date: new Date("2023-03-20")},
+    #   {name: 'Kitkat', quantity: 1, amount: 0.50, date: new Date("2023-03-10")}
+    # ];
+
     def get_permissions(self):
         """
         Instantiates and returns the list of permissions that this view requires.
@@ -106,6 +147,8 @@ class TransactionViewSet(viewsets.ModelViewSet):
         elif self.action == PATCH_PARTIAL_UPDATE:
             permission_classes = [AllowAny]
         elif self.action == DELETE_DESTROY:
+            permission_classes = [AllowAny]
+        elif self.action == "list_transactions":
             permission_classes = [AllowAny]
         else:
             print(self.action)
