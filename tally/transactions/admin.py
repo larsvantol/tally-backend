e0a5fb0f52a906dad80df73756f9af17ed7e9cf9
larsvantol <LMJvanTol@gmail.com>
2023-05-14 11:29:49 +0200

    added fields for uuid and code for authentication

--- tally/transactions/admin.py
+++ tally/transactions/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
@@ -6,101 +7,143 @@ from datetime import datetime
 from .models import Customer, Transaction, SubTransaction, SubPurchase
 import csv
 
+
 # Filter transactions based on dates, day, month, year
 class TransactionDateListFilter(admin.SimpleListFilter):
-    title = 'date'
-    parameter_name = 'date'
+    title = "date"
+    parameter_name = "date"
 
     def lookups(self, request, model_admin):
         return (
-            ('today', 'Today'),
-            ('this_week', 'This week'),
-            ('this_month', 'This month'),
-            ('this_year', 'This year'),
+            ("today", "Today"),
+            ("this_week", "This week"),
+            ("this_month", "This month"),
+            ("this_year", "This year"),
         )
 
     def queryset(self, request, queryset):
-        if self.value() == 'today':
+        if self.value() == "today":
             return queryset.filter(date__date=datetime.today())
-        if self.value() == 'this_week':
+        if self.value() == "this_week":
             return queryset.filter(date__week=datetime.today().isocalendar()[1])
-        if self.value() == 'this_month':
+        if self.value() == "this_month":
             return queryset.filter(date__month=datetime.today().month)
-        if self.value() == 'this_year':
+        if self.value() == "this_year":
             return queryset.filter(date__year=datetime.today().year)
+
 
 class SubTransactionInline(admin.TabularInline):
     model = SubTransaction
     extra = 0
 
+
 class SubPurchaseInline(admin.TabularInline):
     model = SubPurchase
     extra = 0
 
+
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'relation_code')
-    search_fields = ('first_name', 'prefix', 'last_name', 'relation_code')
+    list_display = ("full_name", "relation_code")
+    search_fields = ("first_name", "prefix", "last_name", "relation_code")
     fieldsets = (
-        ('Name', {
-            'fields': ('first_name', 'prefix', 'last_name')
-        }),
-        ('Meta', {
-            'fields': ('created', 'last_modified')
-        }),
-        ('Exact', {
-            'classes': ('collapse',),
-            'fields': ('relation_code',),
-        }),
+        ("Name", {"fields": ("first_name", "prefix", "last_name")}),
+        ("Authentication", {"fields": ("encrypted_uuid", "encrypted_code")}),
+        ("Meta", {"fields": ("created", "last_modified")}),
+        (
+            "Exact",
+            {
+                "classes": ("collapse",),
+                "fields": ("relation_code",),
+            },
+        ),
     )
-    readonly_fields = ('created', 'last_modified')
+    readonly_fields = ("created", "last_modified")
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["encrypted_uuid"].widget = forms.PasswordInput(
+            render_value=True,
+            attrs={"autocomplete": "new-password"},
+        )
+        form.base_fields["encrypted_code"].widget = forms.PasswordInput(
+            render_value=True,
+            attrs={"autocomplete": "new-password"},
+        )
+        return form
+
+    # Hash UUID and code before saving
+    def save_model(self, request, obj, form, change):
+        if not (form.cleaned_data.get("encrypted_uuid") == None):
+            obj.encrypted_uuid = obj.hash_uuid(form.cleaned_data.get("encrypted_uuid"))
+        if not (form.cleaned_data.get("encrypted_code") == None):
+            obj.encrypted_code = obj.hash_code(form.cleaned_data.get("encrypted_code"))
+        super().save_model(request, obj, form, change)
 
 
-@admin.action(description='Export to csv')
+@admin.action(description="Export to csv")
 def export_to_csv(self, request, queryset):
     result = []
     for transaction in queryset:
         result.extend(transaction.export_to_list_exact_format())
 
     response = HttpResponse(
-    content_type="text/csv",
-    headers={"Content-Disposition": 'attachment; filename="export.csv"'}, # TODO: Filename based on export date range and/or settings
+        content_type="text/csv",
+        headers={
+            "Content-Disposition": 'attachment; filename="export.csv"'
+        },  # TODO: Filename based on export date range and/or settings
     )
-    
+
     writer = csv.writer(response)
-    writer.writerow(['nieuwe boeking', 'Omschrijving: Kopregel', 'Factuurdatum', 'Code betalingsconditie', 'Relatiecode', 'Grootboekrekening', 'Omschrijving', 'Aantal', 'Btw-code', 'Bedrag'])
+    writer.writerow(
+        [
+            "nieuwe boeking",
+            "Omschrijving: Kopregel",
+            "Factuurdatum",
+            "Code betalingsconditie",
+            "Relatiecode",
+            "Grootboekrekening",
+            "Omschrijving",
+            "Aantal",
+            "Btw-code",
+            "Bedrag",
+        ]
+    )
     writer.writerows(result)
 
     return response
 
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('transaction_id', 'customer_formatted', 'date', 'total_formatted')
-    ordering = ('-date',)
-    search_fields = ('transaction_id', 'customer__first_name', 'customer__prefix', 'customer__last_name')
+    list_display = ("transaction_id", "customer_formatted", "date", "total_formatted")
+    ordering = ("-date",)
+    search_fields = (
+        "transaction_id",
+        "customer__first_name",
+        "customer__prefix",
+        "customer__last_name",
+    )
     list_filter = (TransactionDateListFilter,)
     inlines = [SubTransactionInline, SubPurchaseInline]
-    autocomplete_fields = ['customer']
+    autocomplete_fields = ["customer"]
     actions = [export_to_csv]
     fieldsets = (
-        ('Information', {
-            'fields': ('transaction_id', 'date', 'customer')
-        }),
-        ('Meta', {
-            'fields': ('created', 'last_modified')
-        }),
+        ("Information", {"fields": ("transaction_id", "date", "customer")}),
+        ("Meta", {"fields": ("created", "last_modified")}),
     )
-    readonly_fields = ('transaction_id', 'created', 'last_modified')
+    readonly_fields = ("transaction_id", "created", "last_modified")
 
-    @admin.display(description='Total')
+    @admin.display(description="Total")
     def total_formatted(self, obj):
         return f"€ {obj.total()}"
 
-    @admin.display(description='Customer')
+    @admin.display(description="Customer")
     def customer_formatted(self, obj):
-        link = reverse(f'admin:{obj.customer._meta.app_label}_{obj.customer._meta.model_name}_change', args=[obj.customer.id])
+        link = reverse(
+            f"admin:{obj.customer._meta.app_label}_{obj.customer._meta.model_name}_change",
+            args=[obj.customer.id],
+        )
         return format_html('<a href="{}">{}</a>', link, str(obj.customer))
 
-    
 
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Transaction, TransactionAdmin)
--- tally/transactions/models.py
+++ tally/transactions/models.py
@@ -1,35 +1,79 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.hashers import make_password, check_password
 from products.models import Product
 import uuid
+
 
 # Create your models here.
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     prefix = models.CharField(max_length=100, blank=True, null=True)
     last_name = models.CharField(max_length=100)
-    relation_code = models.IntegerField(help_text="""Relation code of the customer.""", unique=True)
+    relation_code = models.IntegerField(
+        help_text="""Relation code of the customer.""", unique=True
+    )
+
+    encrypted_uuid = models.CharField(max_length=100, blank=True, null=True)
+    encrypted_code = models.CharField(max_length=100, blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.relation_code) + " - " + self.first_name + " " + (self.prefix + " " if self.prefix else "") + self.last_name
-    
+        return (
+            str(self.relation_code)
+            + " - "
+            + self.first_name
+            + " "
+            + (self.prefix + " " if self.prefix else "")
+            + self.last_name
+        )
+
+    def check_uuid(self, uuid):
+        return check_password(uuid, self.encrypted_uuid)
+
+    def check_code(self, code):
+        return check_password(code, self.encrypted_code)
+
+    def has_uuid(self):
+        return self.encrypted_uuid != ""
+
+    def has_code(self):
+        return self.encrypted_code != ""
+
+    def hash_uuid(self, uuid):
+        return make_password(uuid)
+
+    def hash_code(self, code):
+        return make_password(code)
+
     def full_name(self):
-        return self.first_name + " " + (self.prefix + " " if self.prefix else "") + self.last_name
-    
+        return (
+            self.first_name
+            + " "
+            + (self.prefix + " " if self.prefix else "")
+            + self.last_name
+        )
+
+
 class Transaction(models.Model):
-    transaction_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    transaction_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-    date = models.DateTimeField(default=timezone.now(), help_text="""Date of the transaction.""")
+    date = models.DateTimeField(
+        default=timezone.now(), help_text="""Date of the transaction."""
+    )
 
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
     def total(self):
         total = 0
-        for subtransaction in SubTransaction.objects.filter(transaction=self.transaction_id):
+        for subtransaction in SubTransaction.objects.filter(
+            transaction=self.transaction_id
+        ):
             total += subtransaction.amount
         for subpurchase in SubPurchase.objects.filter(transaction=self.transaction_id):
             total += subpurchase.price * subpurchase.quantity
@@ -42,46 +86,99 @@ class Transaction(models.Model):
         # formatting:
         # nieuwe boeking, omschrijving:kopregel, datum, betalingsconditie, relatiecode, grootboekrekening, omschrijving, aantal, btw code, bedrag
         # TODO: make setting for betalingsconditie (DS)
-        result = [[1, f"tally transaction {self.transaction_id}", self.date.strftime('%d-%m-%Y'), "DS", self.customer.relation_code]]
-        for subtransaction in SubTransaction.objects.filter(transaction=self.transaction_id):
-            result.append(["", "", "", "", "", subtransaction.account_code, subtransaction.description, "", subtransaction.vat_percentage, subtransaction.amount])
+        result = [
+            [
+                1,
+                f"tally transaction {self.transaction_id}",
+                self.date.strftime("%d-%m-%Y"),
+                "DS",
+                self.customer.relation_code,
+            ]
+        ]
+        for subtransaction in SubTransaction.objects.filter(
+            transaction=self.transaction_id
+        ):
+            result.append(
+                [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    subtransaction.account_code,
+                    subtransaction.description,
+                    "",
+                    subtransaction.vat_percentage,
+                    subtransaction.amount,
+                ]
+            )
         for subpurchase in SubPurchase.objects.filter(transaction=self.transaction_id):
-            result.append(["", "", "", "", "", subpurchase.product.account_code, subpurchase.product.name, subpurchase.quantity, subpurchase.product.vat_percentage, subpurchase.amount()])
+            result.append(
+                [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    subpurchase.product.account_code,
+                    subpurchase.product.name,
+                    subpurchase.quantity,
+                    subpurchase.product.vat_percentage,
+                    subpurchase.amount(),
+                ]
+            )
         return result
 
-    
-class SubTransaction(models.Model):
-    description = models.CharField(max_length=100, help_text="""Description of the transaction.""")
-    amount = models.DecimalField(max_digits=10, decimal_places=2, help_text="""Amount to be deducted.""")
-    account_code = models.IntegerField(default=3002, help_text="""account code (grootboekrekeningnummer) for income""")   # TODO: make default account code a setting
-    vat_percentage = models.IntegerField(help_text="""BTW percentage of the transaction.""", default=0) # TODO: make default vat percentage a setting
-    transaction = models.ForeignKey('Transaction', related_name='subtransactions', on_delete=models.CASCADE)
 
+class SubTransaction(models.Model):
+    description = models.CharField(
+        max_length=100, help_text="""Description of the transaction."""
+    )
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, help_text="""Amount to be deducted."""
+    )
+    account_code = models.IntegerField(
+        default=3002, help_text="""account code (grootboekrekeningnummer) for income"""
+    )  # TODO: make default account code a setting
+    vat_percentage = models.IntegerField(
+        help_text="""BTW percentage of the transaction.""", default=0
+    )  # TODO: make default vat percentage a setting
+    transaction = models.ForeignKey(
+        "Transaction", related_name="subtransactions", on_delete=models.CASCADE
+    )
 
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
     def summary(self) -> tuple:
         return (self.description, self.amount)
-    
-    
+
+
 class SubPurchase(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     quantity = models.IntegerField(null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="""Price of the product at the time of purchase.""")
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text="""Price of the product at the time of purchase.""",
+    )
 
-    transaction = models.ForeignKey('Transaction', related_name='subpurchases', on_delete=models.CASCADE)
+    transaction = models.ForeignKey(
+        "Transaction", related_name="subpurchases", on_delete=models.CASCADE
+    )
 
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
     def amount(self):
-        print(f'price: {self.price}, quantity: {self.quantity} = {self.price * self.quantity}')
+        print(
+            f"price: {self.price}, quantity: {self.quantity} = {self.price * self.quantity}"
+        )
         return self.price * self.quantity
-    
+
     def summary(self) -> tuple:
-        return (f'{self.quantity}x {self.product.name}', self.amount())
-    
+        return (f"{self.quantity}x {self.product.name}", self.amount())
+
     def save(self, *args, **kwargs):
         if not self.price:
             self.price = self.product.price
